fcbc671f7731d30e0cc265bdd3a55832cf5c78f1
ZenBarrier <anthony.j.barrera@gmail.com>
2015-11-17 22:57:11 -0500

    modified popup todo angular example to work with chrome storage.

--- JS/popup.js
+++ JS/popup.js
@@ -1,13 +1,28 @@
 angular.module('FBlurApp', [])
-  .controller('TodoListController', function () {
+  .controller('TodoListController', ['$scope', function ($scope) {
       var todoList = this;
       todoList.todos = [
         { text: 'learn angular', done: true },
         { text: 'build an angular app', done: false }];
 
+      chrome.storage.sync.get({
+          storedTodos: []
+      }, function (items) {
+          todoList.todos = items.storedTodos;
+          console.log(items.storedTodos);
+          $scope.$apply();
+      });
+
       todoList.addTodo = function () {
           todoList.todos.push({ text: todoList.todoText, done: false });
           todoList.todoText = '';
+          console.log(todoList.todos);
+
+          chrome.storage.sync.set({
+              storedTodos: todoList.todos
+          }, function (items) {
+              console.log("stored");
+          });
       };
 
       todoList.remaining = function () {
@@ -19,10 +34,22 @@ angular.module('FBlurApp', [])
       };
 
       todoList.archive = function () {
+
           var oldTodos = todoList.todos;
           todoList.todos = [];
+
           angular.forEach(oldTodos, function (todo) {
               if (!todo.done) todoList.todos.push(todo);
+
+              chrome.storage.sync.set({
+                  storedTodos: todoList.todos
+              }, function (items) {
+                  console.log("stored");
+                  $scope.$apply();
+              });
+
           });
+
+
       };
-  });
+  }]);
