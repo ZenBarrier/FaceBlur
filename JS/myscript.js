8d6aa5c9c7e2aa1a47c6736bf55c93668dc9b246
ZenBarrier <anthony.j.barrera@gmail.com>
2015-11-19 18:49:48 -0500

    wrapped myscript to avoid global variable collisions in javascript.

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,18 +1,20 @@
-﻿
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+﻿(function(){
 
-var observer = new MutationObserver(function (mutations, observer) {
-    // fired when a mutation occurs
-    console.log(mutations, observer);
-    $('div a[href^="https://www.facebook.com/rosalie.mii"]').parent().parent().hide();
+    MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-    // ...
-});
+    var observer = new MutationObserver(function (mutations, observer) {
+        // fired when a mutation occurs
+        console.log(mutations, observer);
+        $('div a[href^="https://www.facebook.com/rosalie.mii"]').parent().parent().hide();
 
-// define what element should be observed by the observer
-// and what types of mutations trigger the callback
-observer.observe(document, {
-    subtree: true,
-    attributes: true
-    //...
-});
+        // ...
+    });
+
+    // define what element should be observed by the observer
+    // and what types of mutations trigger the callback
+    observer.observe(document, {
+        subtree: true,
+        attributes: true
+        //...
+    });
+})();
