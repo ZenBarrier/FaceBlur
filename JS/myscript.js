80afec1142907ae239315a040e0d8d6471a5446c
ZenBarrier <anthony.j.barrera@gmail.com>
2015-11-23 23:21:39 -0500

    Added storage change listener to blur when adding new face.

--- JS/myscript.js
+++ JS/myscript.js
@@ -20,15 +20,19 @@
         console.log(faceQuery);
     });
 
-    var observer = new MutationObserver(function (mutations, observer) {
-        // fired when a mutation occurs
+    
 
+    function blurFaces() {
         queryResult = $(faceQuery).not('span > a');
         faceCommentDiv = queryResult.closest('div.UFIComment, div.userContentWrapper');
         faceCommentDiv.find('a').contents().unwrap().wrap('<b></b>');
 
         faceCommentDiv.wrap('<div class="blur"></div>');
+    }
 
+    var observer = new MutationObserver(function (mutations, observer) {
+        // fired when a mutation occurs
+        blurFaces();
     });
 
     // define what element should be observed by the observer
@@ -38,4 +42,20 @@
         attributes: true
         //...
     });
+
+    chrome.storage.onChanged.addListener(function (changes, areaName) {
+        console.log(changes);
+
+        var blockedFacesObjectArray = changes.StoredBlockedFaces.newValue;
+
+        blockedFaces = blockedFacesObjectArray.map(function (item) {
+            return item['profile'];
+        });
+
+        faceQuery = 'a[href^="' + blockedFaces.join('"], a[href^="') + '"]';
+        console.log(faceQuery);
+
+        blurFaces();
+    });
+
 })();
