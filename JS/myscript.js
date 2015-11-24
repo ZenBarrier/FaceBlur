39d7f390863a78242944461917a9f842534b2d08
ZenBarrier <anthony.j.barrera@gmail.com>
2015-11-23 22:47:28 -0500

    Fixes bug when others tag a blocked person. Now also blurs posts.

--- JS/myscript.js
+++ JS/myscript.js
@@ -23,8 +23,8 @@
     var observer = new MutationObserver(function (mutations, observer) {
         // fired when a mutation occurs
 
-        queryResult = $(faceQuery);
-        faceCommentDiv = queryResult.closest('div.UFIComment');
+        queryResult = $(faceQuery).not('span > a');
+        faceCommentDiv = queryResult.closest('div.UFIComment, div.userContentWrapper');
         faceCommentDiv.find('a').contents().unwrap().wrap('<b></b>');
 
         faceCommentDiv.wrap('<div class="blur"></div>');
