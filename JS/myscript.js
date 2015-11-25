0fcba74f82f24ee5ced6d799fde80f90db7e1a06
ZenBarrier <anthony.j.barrera@gmail.com>
2015-11-25 00:52:37 -0500

    Unblur and blur facebook content actively.

--- JS/myscript.js
+++ JS/myscript.js
@@ -4,6 +4,8 @@
 
     var blockedFaces = [];
     var faceQuery = '';
+    var unblockedFaces = [];
+    var unbluredQuery = '';
 
     chrome.storage.sync.get({
         StoredBlockedFaces: []
@@ -25,11 +27,25 @@
     
 
     function blurFaces() {
-        queryResult = $(faceQuery).not('span > a');
-        faceCommentDiv = queryResult.closest('div.UFIComment, div.userContentWrapper');
-        faceCommentDiv.find('a').contents().unwrap().wrap('<b></b>');
+        var queryResult = $(faceQuery).not('span > a');
+        var faceCommentDiv = queryResult.closest('div.UFIComment, div.userContentWrapper');
+        //faceCommentDiv.find('a').contents().unwrap().wrap('<b></b>');
+        faceCommentDiv.find('a').each(function () {
+            var oldLink = $(this).attr('href');
+            $(this).contents().unwrap().wrap('<b oldhref="'+oldLink+'"></b>');
+        });
 
         faceCommentDiv.wrap('<div class="blur"></div>');
+    }
+
+    function unblurFaces() {
+        var queryResult = $(unbluredQuery);
+        var blurDiv = queryResult.closest('div.blur');
+        blurDiv.find('b').each(function () {
+            var oldLink = $(this).attr('oldhref');
+            $(this).contents().unwrap().wrap('<a href="' + oldLink + '"></a>');
+        });
+        blurDiv.contents().unwrap();
     }
 
     var observer = new MutationObserver(function (mutations, observer) {
@@ -50,16 +66,24 @@
 
         var blockedFacesObjectArray = changes.StoredBlockedFaces.newValue;
 
+        var oldBlockedFaces = blockedFaces;
+
         blockedFaces = blockedFacesObjectArray.filter(function (face, index, array) {
             return (face.blur);
         }).map(function (item) {
             return item['profile'];
         });
+        unblockedFaces = oldBlockedFaces.concat(unblockedFaces).filter(function (profile, index, array) {
+            return blockedFaces.indexOf(profile)==-1;
+        });
+        console.log(unblockedFaces)
+        unbluredQuery = 'b[oldhref^="' + unblockedFaces.join('"], b[oldhref^="') + '"]';
 
         faceQuery = 'a[href^="' + blockedFaces.join('"], a[href^="') + '"]';
         console.log(faceQuery);
 
         blurFaces();
+        unblurFaces();
     });
 
 })();
